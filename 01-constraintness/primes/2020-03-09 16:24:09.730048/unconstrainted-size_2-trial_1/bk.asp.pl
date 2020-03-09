35c4fe314336a95dfd7808754c8dbdd8a780c74c
polyphemus <rolfmorel@gmail.com>
2020-03-09 16:56:42 +0000

    Constraintness primes experiment
    For some reason considers exact same number of programs, no matter the constraints.
    
    Maybe the ordering already picks out all the sensible ones

---
+++ 01-constraintness/primes/2020-03-09 16:24:09.730048/unconstrainted-size_2-trial_1/bk.asp.pl
@@ -0,0 +1,8 @@
+modeh(f,1).
+direction(f,0,in).
+
+modeb(div2,1).
+direction(div2,0,in).
+modeb(div3,1).
+direction(div3,0,in).
+
---
+++ 01-constraintness/primes/2020-03-09 16:24:09.730048/unconstrainted-size_7-trial_2/parameters.pl
@@ -0,0 +1,5 @@
+
+max_vars(8).
+max_body(10).
+max_clauses(1).
+nr_bottoms_(0).
